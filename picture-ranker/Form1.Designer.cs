b9ed22960ca7bd656a17afa97d3f28e8c49bb50a
mirza <46076246+mirzapolat@users.noreply.github.com>
2023-04-29 22:32:28 +0200

    First commit

---
+++ picture-ranker/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace picture_ranker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.initializeFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pb1.Location = new System.Drawing.Point(35, 79);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(800, 800);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 2;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnLoad,
+            this.btnSave,
+            this.initializeFolderToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1706, 48);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(140, 36);
+            this.btnLoad.Text = "Load New";
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(84, 36);
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pb2.Location = new System.Drawing.Point(866, 79);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(800, 800);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb2.TabIndex = 4;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // initializeFolderToolStripMenuItem
+            // 
+            this.initializeFolderToolStripMenuItem.Name = "initializeFolderToolStripMenuItem";
+            this.initializeFolderToolStripMenuItem.Size = new System.Drawing.Size(196, 44);
+            this.initializeFolderToolStripMenuItem.Text = "Initialize Folder";
+            this.initializeFolderToolStripMenuItem.Click += new System.EventHandler(this.initializeFolderToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1706, 911);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Picture Ranker";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnLoad;
+        private System.Windows.Forms.ToolStripMenuItem btnSave;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.ToolStripMenuItem initializeFolderToolStripMenuItem;
+    }
+}
+
---
+++ picture-ranker/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace picture_ranker
+{
+    public partial class Form1 : Form
+    {
+        bool started = false;
+        Dictionary<string, int> map = new Dictionary<string, int>();
+        Random random = new Random();
+        string path = "";
+
+        string a = "";
+        string b = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pb1_Click(object sender, EventArgs e)
+        {
+            int act = map[a];
+            map[a] = act + 1;
+
+            nextImages();
+        }
+
+        private void pb2_Click(object sender, EventArgs e)
+        {
+            int act = map[b];
+            map[b] = act + 1;
+
+            nextImages();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult res = dialog.ShowDialog();
+            if (res != DialogResult.OK) return;
+            path = dialog.SelectedPath + "/scores.txt";
+
+            var enumLines = File.ReadLines(dialog.SelectedPath + "/scores.txt", Encoding.UTF8);
+
+            foreach (var line in enumLines)
+            {
+                map.Add(line.Substring(line.Split(' ')[0].Length + 1), Int32.Parse(line.Split(' ')[0]));
+            }
+
+            nextImages();
+        }
+
+        private void nextImages()
+        {
+            a = map.ElementAt(random.Next(0, map.Count)).Key;
+            do
+            {
+                b = map.ElementAt(random.Next(0, map.Count)).Key;
+            } while (a == b);
+
+            pb1.ImageLocation = a;
+            pb2.ImageLocation = b;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            StreamWriter strm = File.CreateText(path);
+            strm.Flush();
+            foreach (var item in map.OrderByDescending(key => key.Value))
+            {
+                strm.WriteLine(item.Value.ToString() + " " + item.Key);
+            }
+            strm.Close();
+        }
+
+        private void initializeFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult res = dialog.ShowDialog();
+
+            if (res != DialogResult.OK) return;
+
+            string[] files = Directory.GetFiles(dialog.SelectedPath);
+
+            StreamWriter sw = new StreamWriter(dialog.SelectedPath + "/scores.txt");
+            foreach (string file in files)
+            {
+                sw.WriteLine("0 " + file);
+            }
+            sw.Close();
+        }
+    }
+}
